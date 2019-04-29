e6b87ae31d2ebf2c1101bf8553ac1b3e51370bcf
carisami <nacal1998@gmail.com>
2019-04-29 13:37:28 +0200

    funciones

--- JS/20_asincrono.js
+++ JS/20_asincrono.js
@@ -1,14 +1,18 @@
 /**
- * La funcion del setTimeout
- * permite un proceso asincrono, diferido en el tiempo
+ * La función setTimeout
+ * permite un proceso asíncrono, diferido en el tiempo
  */
 
 
- console.log ('Primero')
-/* function 
- //dentro de un rato
- //setTimeout (paraLuego, 2000)
+console.log('Primero')
+
+/* function paraLuego () {
+     console.log('Segundo')
+ } */
+
+ // dentro de un rato 
+ // setTimeout( paraLuego, 2000)
  setTimeout( () => {
-      console.log('Segundo')
- },2000)
- console.log('Tercero')
+    console.log('Segundo')
+ }, 2000)
+  console.log('Tercero')
