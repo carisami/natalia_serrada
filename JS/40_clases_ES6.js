8ad689c240e43ef8ba49df8d5afc370773010a34
carisami <nacal1998@gmail.com>
2019-05-13 10:53:57 +0200

    Herencia y clases en ES6

--- JS/40_clases_ES6.js
+++ JS/40_clases_ES6.js
@@ -1,15 +1,15 @@
-    class Persona {
-        constructor(nombre, edad) {
-            this.nombre = nombre
-            this.edad = edad
-            this.alma = true
-        }
-        saludar (aQuien = 'amigo') {
-            console.log(`Hola ${aQuien}, soy ${this.nombre}`)
-        }
+class Persona {
+    constructor (nombre, edad) {
+        this.nombre = nombre
+        this.edad = edad
+        this.alma = true
     }
+    saludar (aQuien = 'amigo') {
+        console.log(`Hola ${aQuien}, soy ${this.nombre}`)
+    }
+}
 
-person1 = new Persona ('Pepe', 23)
+person1 = new Persona('Pepe', 23)
 
 person1.altura = 175
 
@@ -26,6 +26,10 @@ class Alumno extends Persona {
         console.log(`Estoy estudiando ${this.curso}`)
     }
 }
+
+alumno1 = new Alumno('Elena', 22, 'WordPress')
+console.log(alumno1)
+alumno1.saludar()
 
 alumno1 = new Alumno ('Elena', 22, 'WordPress')
 console.log(alumno1)
