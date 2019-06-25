853cc55e731e0611a2a0f99bddcb5e62c7d2affa
carisami <nacal1998@gmail.com>
2019-06-25 09:29:46 +0200

    componete saludar

--- angular-cife/src/app/app.module.ts
+++ angular-cife/src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { HomeModule } from './home/home.module';
-import { TareasModule } from './tareas/tareas.module';
-import { ContactosModule } from './contactos/contactos.module';
-import { LibrosModule } from './libros/libros.module';
 
 @NgModule({
   declarations: [
@@ -15,12 +11,9 @@ import { LibrosModule } from './libros/libros.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    CoreModule,
-    HomeModule,
-    TareasModule,
-    ContactosModule,
-    LibrosModule
+    CoreModule
   ],
   providers: [],
   exports: [],
